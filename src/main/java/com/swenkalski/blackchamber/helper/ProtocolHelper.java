4992a0963b5bc9f094ef8a12a9acb1dc73c5d8e6
Swen Kalski <kalski.swen@gmail.com>
2021-01-09 14:31:08 +0100

    Add IP4 to ProtocolHelper in fact taht IP Handles are used for private and development

--- src/main/java/com/swenkalski/blackchamber/helper/ProtocolHelper.java
+++ src/main/java/com/swenkalski/blackchamber/helper/ProtocolHelper.java
@@ -1,17 +1,33 @@
 package com.swenkalski.blackchamber.helper;
 
+import java.util.regex.Pattern;
+
 public class ProtocolHelper {
 
     public static final String LOCALHOST = "localhost";
     public static final String HTTP = "http://";
     public static final String HTTPS = "https://";
 
-    public static String getProtocol(String host){
-        if(host.equals(LOCALHOST)){
+    private static final String zeroTo255
+            = "([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])";
+
+    private static final String IP_REGEXP
+            = zeroTo255 + "\\." + zeroTo255 + "\\."
+            + zeroTo255 + "\\." + zeroTo255;
+
+    private static final Pattern IP_PATTERN
+            = Pattern.compile(IP_REGEXP);
+
+    public static String getProtocol(String host) {
+        if (host.equals(LOCALHOST) || isValid(host)) {
             return HTTP;
         }
 
         return HTTPS;
+    }
+
+    private static boolean isValid(String address) {
+        return IP_PATTERN.matcher(address).matches();
     }
 
 }
